c098b316a880e714d7fea5bdcc850405f6ba5e60
AdamDono <flipbrickzmusic1@gmail.com>
2025-03-18 10:58:41 +0200

    pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,1 @@
-import { Component } from '@angular/core';
-import { Router, Event, NavigationStart, NavigationEnd, NavigationError, RouterOutlet } from '@angular/router';
-@Component({
-  standalone: true,
-  imports: [RouterOutlet],
-  selector: 'app-root',
-  template: `
-    <main class="container">
-      <h1>Movie App</h1>
-      <router-outlet></router-outlet>
-    </main>
-  `,
-})
-export class AppComponent {
-  constructor(private router: Router) {
 
-  ;
-  }
-}
