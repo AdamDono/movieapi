548cb530f123e5c38027868291e76c74536045e0
AdamDono <flipbrickzmusic1@gmail.com>
2025-03-22 10:28:28 +0200

    fixed gitignore for deploying

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    tmdbApiKey: '67b1d9c5f9253c22e3714162391cecee' ,// Replace with your key
+     apiUrl: 'https://api.themoviedb.org/3'
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    tmdbApiKey: '67b1d9c5f9253c22e3714162391cecee' ,// Same key or use a production-specific key
+      apiUrl: 'https://api.themoviedb.org/3'
+  };
